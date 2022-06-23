3b1e93428bb71a8e9bf3a76cbbd5cc34a0f8ea3c
saki <sakiranbo39@gamil.com>
2022-06-23 15:01:59 +0900

    first commit

---
+++ src/main/java/com/example/RegistUserApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistUserApplication.class, args);
+	}
+
+}
